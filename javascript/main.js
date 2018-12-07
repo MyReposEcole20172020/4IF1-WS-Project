292ded7c4d74d007db6bce1b5df6eb8c83912531
gooluke <gustavo-chaiza-ramirez@outlook.fr>
2018-12-07 14:54:55 +0100

    adding

--- javascript/main.js
+++ javascript/main.js
@@ -19,7 +19,7 @@ $("document").ready(function() {
 
   var query = 'SELECT DISTINCT ?pays WHERE {?pays dbp:populationCensus ?population}';
   var newquery = encodeURIComponent(query);
-  var url = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query='+ query +'&output=json';
+  var url = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query='+ newquery +'&output=json';
   $.getJSON(url+"&callback=?", function(resultats) {
     $(resultats.results.bindings).each(function(i) {
       $('#result').append("<p>" + resultats.results.bindings[i].pays.value + "</p>");
