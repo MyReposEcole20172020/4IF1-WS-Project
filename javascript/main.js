460ea2355b10bb3f0a39fc2860497d43e8b002e7
gooluke <gustavo-chaiza-ramirez@outlook.fr>
2018-12-13 18:44:48 +0100

    Adding subject and group to description

--- javascript/main.js
+++ javascript/main.js
@@ -3,12 +3,10 @@
 var autocompleteOptions = [];
 var mapping = [];
 
-var data;
-
 $("document").ready(function() {
     
-  $("#result").css("color","white");
-  $("#description").hide();
+  $("#description").css("color","white");
+  $("#result").html($("#description"));
 
   $( function() {   
     $( "#search" ).autocomplete({
@@ -20,25 +18,6 @@ $("document").ready(function() {
   
   setAutocompleteOptions();
 
-  /* Get variable sent from index html */
-  var image = parent.document.URL.substring(parent.document.URL.indexOf('=')+1, parent.document.URL.length);
-  image = image.replace(/%20/g,' ');
-  var imagequery = findURI(mapping,image);
-  imagequery = 'SELECT DISTINCT ?image WHERE {<' + imagequery + '> foaf:depiction ?image.}';
-  console.log("\n\n\n" +imagequery + "\n\n\n");
-  imagequery = encodeURIComponent(imagequery);
-    var myurl = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query='+ imagequery +'&output=json';
-    $.getJSON(myurl+"&callback=?", function(resultats) {
-    if(resultats.results.bindings.length == 0 ){
-      $("#result").append('<p>Image not found</p>');    
-    } else {
-      $(resultats.results.bindings).each(function(i) {
-        var imagesrc = resultats.results.bindings[i].image.value;
-        $("#result").append('<img src="'+ imagesrc +'">');
-      });
-    }
-  });
-
   $('#search').keypress(function(e){
     if(e.keyCode==13){
       e.preventDefault();
@@ -47,8 +26,6 @@ $("document").ready(function() {
   });
 
 });
-
-
 
 /* Search value */
 var value;
@@ -59,31 +36,6 @@ $("#search").keyup(function() {
 function autoCompleteSelectHandler(event, ui) {               
   value = ui.item.value;
 }
-
-/* Query */
-
-/*var query = 'SELECT DISTINCT ?pays WHERE {?pays dbp:populationCensus ?population}';
-var newquery = encodeURIComponent(query);
-var url = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query='+ newquery +'&output=json';
-$.getJSON(url+"&callback=?", function(resultats) {
-  $(resultats.results.bindings).each(function(i) {
-    var item = resultats.results.bindings[i].pays.value;
-    item = item.substring(item.lastIndexOf('/') + 1);
-    $('#result').append("<p>" + item + "</p>");
-    
-  });
-});*/
-
-/*
-var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-      $.getJSON(flickerAPI, {
-          tags: "mount everest",
-          tagmode: "any",
-          format: "json"
-      }, function (data) {
-          alert("success");
-      });
-      return false;*/
 
 /* Redirecting */
 
@@ -107,39 +59,7 @@ function goToMap() {
   document.location.href = url;
 }
 
-function displayTable() {
-  $("#description").show();
-  $("#result").html($("#description"));
-}
-
 /* Retrieving data from XML file */
-
-/*
-var url = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query='+ newquery +'&output=xml&callback=?';
-function getXmlFromFile() {
-  $.ajax({
-    type: "GET",
-    crossDomain: true,
-    url: url,
-    datatype: "xml",
-    success: createXmlFile,
-    error: loadfail});
-}
-
-function createXmlFile(data){
-  $(data).find('uri').each(function(){
-    var uri = $(this).find('uri').text();
-    autocompleteOptions.push(uri);
-  });
-}*/
-   
-/*function setAutocompleteOptions(data) {
-  $(data).find('creature').each(function(){
-    var id = $(this).attr('id');
-    var name = $(this).find('name').text();
-    autocompleteOptions.push(name);
-  });
-}*/
 
 function readXML(){
   var mYxmlObject = new XMLHttpRequest();
@@ -152,7 +72,6 @@ function setAutocompleteOptions() {
   var data = readXML();
   $(data).find('uri').each(function(i){
     var text = $(this).text();
-    $('this').attr('id', i);
     var name = text.substring(text.lastIndexOf('/') + 1);
     if(name.indexOf('(') != -1) {
       name = name.substring(0,name.indexOf('('));
---
+++ javascript/requests.js
@@ -0,0 +1,118 @@
+/* Filling mapping */
+var map = [];
+
+var val = parent.document.URL.substring(parent.document.URL.indexOf('=') + 1, parent.document.URL.length); //Get variable in URL
+val = val.replace(/%20/g, ' '); // Replace %20 strings with white spaces
+
+$("document").ready(function () {
+
+    fillMapping();
+    var inputURI = findURI2(map, val);
+
+    insertImage(inputURI);
+    insertSubject(inputURI);
+    insertGroup(inputURI);
+    insertComment(inputURI);
+    insertGender(inputURI);
+    insertBasedOn(inputURI);
+    insertFirstAppearance(inputURI);
+    insertCountry(inputURI);
+    insertRegion(inputURI);
+    insertHabitat(inputURI);
+    insertMythology(inputURI);
+    insertSimilarCreatures(inputURI);
+
+});
+    
+function insertImage(subject) {
+
+    var query = 'SELECT DISTINCT ?out WHERE {<' + subject + '> ' + 'foaf:depiction' + ' ?out.}';
+    console.log("\n\n\n" + query + "\n\n\n");
+    query = encodeURIComponent(query);
+    var myurl = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query='+ query +'&output=json';
+
+    $.getJSON(myurl + "&callback=?", function(resultats) {
+
+        if(resultats.results.bindings.length == 0 ) {
+            $('#image').append('<span>Image not found</span>'); 
+        } else {
+            $(resultats.results.bindings).each( function(i) {
+                var result = resultats.results.bindings[i].out.value;
+                $("#image").append("<img src='" + result + "' />");
+          });
+        }
+        
+      });
+}
+
+function insertSubject(subject) {
+
+    var query = 'SELECT DISTINCT ?out WHERE {<' + subject + '> ' + 'dct:subject' + ' ?out.}';
+    console.log("\n\n\n" + query + "\n\n\n");
+    query = encodeURIComponent(query);
+    var myurl = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=' + query + '&output=json';
+
+    $.getJSON(myurl + "&callback=?", function (resultats) {
+
+        if (resultats.results.bindings.length == 0) {
+            $('#subject').append('<span>Subject not found</span>');
+        } else {
+            $(resultats.results.bindings).each(function (i) {
+                var result = resultats.results.bindings[i].out.value;
+                result = result.substring(result.lastIndexOf(':') + 1);
+                result = result.replace(/_/g, ' ');
+                $("#subject").append("<item>" + result + "<br /><item>");
+            });
+        }
+
+    });
+}
+
+function insertGroup(subject) {
+
+    var query = 'SELECT DISTINCT ?out WHERE {<' + subject + '> ' + 'dbp:grouping' + ' ?out.}';
+    console.log("\n\n\n" + query + "\n\n\n");
+    query = encodeURIComponent(query);
+    var myurl = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=' + query + '&output=json';
+
+    $.getJSON(myurl + "&callback=?", function (resultats) {
+
+        if (resultats.results.bindings.length == 0) {
+            $('#group').append('<span>Group not found</span>');
+        } else {
+            $(resultats.results.bindings).each(function (i) {
+                var result = resultats.results.bindings[i].out.value;
+                result = result.substring(result.lastIndexOf('/') + 1);
+                result = result.replace(/_/g, ' ');
+                $("#group").append("<item>" + result + "<br /><item>");
+            });
+        }
+
+    });
+}
+
+function findURI2(map, value) {
+    for (var i in map) {
+        if (map[i].key == value) {
+            return map[i].uri;
+        }
+    }
+    return 0;
+}
+
+function fillMapping() {
+    var data = readXML();
+    $(data).find('uri').each(function (i) {
+        var uri = $(this).text();
+        var name = uri.substring(uri.lastIndexOf('/') + 1);
+        if (name.indexOf('(') != -1) {
+            name = name.substring(0, name.indexOf('('));
+        }
+        if (name.lastIndexOf('_') == (name.length - 1)) {
+            name = name.substring(0, name.lastIndexOf('_'));
+        }
+        name = name.replace(/_/g, ' ');
+        var object = { 'key': name, 'uri': uri };
+        map.push(object);
+    });
+}
